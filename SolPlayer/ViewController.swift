0dae7e654256f88966e74055fde17a4b1e9fdfc9
Morioka Naoya <g1989n@gmail.com>
2016-05-29 22:49:10 +0900

    Initial Commit

---
+++ SolPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolPlayer
+//
+//  Created by Morioka Naoya on H28/05/29.
+//  Copyright © 平成28年 Morioka Naoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SolPlayerTests/SolPlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SolPlayerTests.swift
+//  SolPlayerTests
+//
+//  Created by Morioka Naoya on H28/05/29.
+//  Copyright © 平成28年 Morioka Naoya. All rights reserved.
+//
+
+import XCTest
+@testable import SolPlayer
+
+class SolPlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
